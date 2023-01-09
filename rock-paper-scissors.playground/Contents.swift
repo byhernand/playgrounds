6bd113f64ba9fbb9d1fa5aa7d38df5ef66e1c785
Eduardo Hernand <byhernand@hotmail.com>
2023-01-09 17:16:09 -0600

    Rock, paper and scissors playground

---
+++ rock-paper-scissors.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+let rock = "rock"
+let paper = "paper"
+let scissors = "scissors"
+
+
+func randomOption() -> String {
+    let options = [rock, paper, scissors]
+    let randomNum = Int.random(in: 0..<options.count)
+    return options[randomNum]
+}
+
+
+func play(user: String) {
+    let pc = randomOption()
+    var result : String
+    
+    print("You choose \"\(user)\".")
+
+    switch user {
+    case pc:
+        print("PC choose \"\(pc)\", it's a draw!")
+    case rock:
+        result = pc == scissors ? "win" : "lose"
+        print("PC choose \"\(pc)\", you \(result)!")
+    case paper:
+        result = pc == rock ? "win" : "lose"
+        print("PC choose \"\(pc)\", you \(result)!")
+    case scissors:
+        result = pc == paper ? "win" : "lose"
+        print("PC choose \"\(pc)\", you \(result)!")
+    default:
+        print("Wrong option")
+    }
+}
+
+play(user: rock)
