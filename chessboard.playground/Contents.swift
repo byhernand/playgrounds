da9bd7aaadf2b682cc8374b9679981ac6cef8111
Eduardo Hernand <byhernand@hotmail.com>
2023-01-14 18:56:28 -0600

    Updated chessboard: Adding UIView

--- chessboard.playground/Contents.swift
+++ chessboard.playground/Contents.swift
@@ -1,24 +1,32 @@
 import UIKit
+import PlaygroundSupport
 
-var chessboard = String()
+let vc = UIViewController()
+
+let chessboard = UILabel(frame: CGRect(x: 300, y: 200, width: 200, height: 300))
+chessboard.text = ""
+chessboard.numberOfLines = 0
+
+let description = UILabel(frame: CGRect(x: 276, y: 440, width: 250, height: 50))
+description.text = "Chessboard with Swift and UIkit"
+
 var initialPattern = true
 
 for num in  1...64 {
     if initialPattern {
-        chessboard += num % 2 == 0 ? "⬛️" : "⬜️"
+        chessboard.text! += num % 2 == 0 ? "⬛️" : "⬜️"
     } else {
-        chessboard += num % 2 == 0 ? "⬜️" : "⬛️"
+        chessboard.text! += num % 2 == 0 ? "⬜️" : "⬛️"
     }
 
     // Line break
     if num % 8 == 0 {
-        chessboard += "\n"
+        chessboard.text! += "\n"
         initialPattern = !initialPattern
     }
 }
 
-print("""
-\(chessboard)\
-
-Chessboard with Swift
-""")
+vc.view.addSubview(chessboard)
+vc.view.addSubview(description)
+vc.view.backgroundColor = .lightGray
+PlaygroundPage.current.liveView = vc
