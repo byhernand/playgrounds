b7f4be744f91b135d09689908d637ca0aebd4338
Eduardo Hernand <byhernand@hotmail.com>
2023-01-27 15:06:30 -0600

    Updated prime number

--- prime-number.playground/Contents.swift
+++ prime-number.playground/Contents.swift
@@ -3,24 +3,32 @@ import Foundation
 
 func isPrime(_ num: Int) -> Bool {
     let primeNumbers = [2,3,5,7]
-    var calc = true
     
-    for prime in primeNumbers {
-        if primeNumbers.contains(num) {
-            break
-        }
-        if num % prime == 0 || num == 1 {
-            calc = false
-            break
+    if num <= 1 {
+        return false
+    }
+
+    if primeNumbers.contains(num) {
+        return true
+    }
+
+    for primeNum in primeNumbers {
+        if num % primeNum == 0 {
+            return false
         }
     }
     
-    return calc
+    return true
 }
 
+
+var result = String()
 
 for number in 1...100 {
     if isPrime(number) {
-        print(number)
+        result.append("\(number),")
     }
 }
+
+
+print(result.dropLast())
