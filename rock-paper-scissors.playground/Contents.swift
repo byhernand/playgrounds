c59e79c4d960c56f78387aca408e6c1f39f7d967
Eduardo Hernand <byhernand@hotmail.com>
2023-03-06 18:16:42 -0600

    Updated rock, paper and scissors: Enumerations

--- rock-paper-scissors.playground/Contents.swift
+++ rock-paper-scissors.playground/Contents.swift
@@ -1,39 +1,37 @@
 import Foundation
 
 
-let rock = "rock"
-let paper = "paper"
-let scissors = "scissors"
-
-
-func randomChoice() -> String {
-    let options = [rock, paper, scissors]
-    let randomIdx = Int.random(in: 0..<options.count)
-    return options[randomIdx]
+enum Choices {
+    case rock, paper, scissors
 }
 
 
-func play(userChoice: String) {
+func randomChoice() -> Choices {
+    let options: [Choices] = [.rock, .paper, .scissors]
+    let randomOption = Int.random(in: 0..<options.count)
+    return options[randomOption]
+}
+
+
+func play(with userChoice: Choices) {
     let pcChoice = randomChoice()
     var result = String()
 
     switch userChoice {
-    case pcChoice:
-        result = "It's a draw"
-    case rock:
-        result = pcChoice == scissors ? "You win" : "You lose"
-    case paper:
-        result = pcChoice == rock ? "You win" : "You lose"
-    case scissors:
-        result = pcChoice == paper ? "You win" : "You lose"
-    default:
-        result = "Null choice, try again"
+        case pcChoice:
+            result = "It's a draw"
+        case .rock:
+            result = pcChoice == .scissors ? "You win" : "You lose"
+        case .paper:
+            result = pcChoice == .rock ? "You win" : "You lose"
+        case .scissors:
+            result = pcChoice == .paper ? "You win" : "You lose"
     }
 
-    print("🦾 PC choose \"\(pcChoice)\"")
     print("💪🏼 You choose \"\(userChoice)\"")
+    print("🦾 PC choose \"\(pcChoice)\"")
     print("🗣️ \(result)!")
 }
 
 
-play(userChoice: rock)
+play(with: .rock)
