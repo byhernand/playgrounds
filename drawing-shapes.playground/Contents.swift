8a5b5cd0b3591ea6a386abf391698aa9f40f97cd
Eduardo Hernand <byhernand@hotmail.com>
2023-03-21 22:02:48 -0600

    Updated drawing shapes: Adding triangle

--- drawing-shapes.playground/Contents.swift
+++ drawing-shapes.playground/Contents.swift
@@ -1,17 +1,41 @@
 import Foundation
 
+
 enum Polygon: String {
-    case square
+    case square, triangle
 }
 
-func drawShape(width: Int, shape: Polygon) {
-    var length = 1...width
 
-    for _ in length {
-        for n in length {
-            print("* ", terminator: n == width ? "\n" : "")
+func drawShape(width: Int, shape: Polygon) {
+    func drawSquare() {
+        var length = 1...width
+        for _ in length {
+            for n in length {
+                print("* ", terminator: n == width ? "\n" : "")
+            }
         }
+    }
+    
+    func drawTriangle() {
+        var pieces = "*"
+        var i = 1
+
+        while i <= width {
+            print(pieces)
+            pieces.insert(contentsOf: " *", at: pieces.endIndex)
+            i += 1
+        }
+    }
+
+    switch shape {
+        case .square:
+            drawSquare()
+        case .triangle:
+            drawTriangle()
     }
 }
 
-drawShape(width: 4, shape: .square)
+
+drawShape(width: 6, shape: .square)
+print("")
+drawShape(width: 6, shape: .triangle)
