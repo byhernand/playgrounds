73dd6d9a8045621bf394aaa9294898500b81e5cf
Eduardo Hernand <byhernand@hotmail.com>
2023-04-14 18:37:34 -0600

    Updated sexagenary playground

--- sexagenary-cycle.playground/Contents.swift
+++ sexagenary-cycle.playground/Contents.swift
@@ -1,49 +1,24 @@
 import Foundation
 
 
-enum Animal: String, CaseIterable {
-    case rat, ox, tiger, rabbit, dragon, snake,
-         horse, sheep, monkey, rooster, dog, pig
-}
+func chineseZodiacSign(year: Int) -> String {
+    let animals = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "rooster", "dog", "pig"]
+    let elements = ["wood", "fire", "earth", "metal", "water"]
 
-enum Element: String, CaseIterable {
-    case wood, fire, earth, metal, water
-}
-
-
-func chineseZodiacSign(_ year: Int) -> (String, String) {
-    var animalsTable = [String]()
-    var elementsTable = [String]()
-    let currentCycle = 1984
-    let nextCycle = 2044
-    var sixtyYearsCycle = currentCycle
-
-    if year > nextCycle {
-        while year > (sixtyYearsCycle + 60) { sixtyYearsCycle += 60 }
-    } else {
-        while year < sixtyYearsCycle { sixtyYearsCycle -= 60 }
+    if year < 604 {
+        return "The sexagenary cycle started in the year 604."
     }
 
-    while animalsTable.count < 60 {
-        for animal in Animal.allCases {
-            animalsTable.append(animal.rawValue)
-        }
-    }
+    let sexagenaryYear = (year - 4) % 60
+    let element = elements[(sexagenaryYear % 10) / 2]
+    let animal = animals[sexagenaryYear % 12]
 
-    while elementsTable.count < 60 {
-        for element in Element.allCases {
-            elementsTable.append(element.rawValue)
-            elementsTable.append(element.rawValue)
-        }
-    }
-
-    let position = year - sixtyYearsCycle
-
-    return (elementsTable[position], animalsTable[position])
+    return "\(year): \(element) \(animal)"
 }
 
 
-print(chineseZodiacSign(2023))
-print(chineseZodiacSign(1924))
-print(chineseZodiacSign(2000))
-print(chineseZodiacSign(2050))
+print(chineseZodiacSign(year: 2023))
+print(chineseZodiacSign(year: 1924))
+print(chineseZodiacSign(year: 2000))
+print(chineseZodiacSign(year: 2050))
+print(chineseZodiacSign(year: 2096))
