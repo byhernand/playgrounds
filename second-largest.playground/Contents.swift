6f9a9480d8a698aa4c31a4e04968bfdf4a1dbd59
Eduardo Hernand <byhernand@hotmail.com>
2023-04-17 15:00:00 +0100

    Updated second largest

--- second-largest.playground/Contents.swift
+++ second-largest.playground/Contents.swift
@@ -3,19 +3,21 @@ import Foundation
 /* Challenge: Find the second largest number in a list. */
 
 func secondGreater(list: [Int]) -> Int? {
-    let list = list.sorted(by: >)
-    
-    if !list.isEmpty {
-        let differentNumbers = list.filter { $0 != list[0] }
-        if differentNumbers.isEmpty {
-            return nil // The list contains the same number repeated
+    let numbers = list.sorted(by: >)
+    var mySet : Set<Int> = []
+
+    for num in numbers {
+        if mySet.contains(num) {
+            return nil // The list has duplicate numbers
+        } else {
+            mySet.insert(num)
         }
     }
 
-    return list.count >= 2 ? list[1] : nil
+    return numbers.count >= 2 ? numbers[1] : nil
 }
 
-print(secondGreater(list: [1,4,5,7,8,9])  ?? "There aren't number.")
-print(secondGreater(list: [15,24,97,102,74])  ?? "There aren't number.")
-print(secondGreater(list: [4,4]) ?? "There aren't number.")
-print(secondGreater(list: [1]) ?? "There aren't number.")
+print(secondGreater(list: [1,4,5,7,8,9])  ?? "There isn't a number.")
+print(secondGreater(list: [15,24,97,102,74])  ?? "There isn't a number.")
+print(secondGreater(list: [4,4]) ?? "There isn't a number.")
+print(secondGreater(list: [1]) ?? "There isn't a number.")
