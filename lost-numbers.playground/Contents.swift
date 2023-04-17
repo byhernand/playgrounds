1a12b8fb970d86241eceec223ac3b3d5f93cb3cc
Eduardo Hernand <byhernand@hotmail.com>
2023-04-16 23:08:05 -0600

    Updated lost numbers

--- lost-numbers.playground/Contents.swift
+++ lost-numbers.playground/Contents.swift
@@ -1,44 +1,53 @@
 import Foundation
 
 
-enum NumberError: Error {
-    case notEnoughElements, duplicate
+enum ListError: Error {
+    case notEnoughItems, duplicateItems
 }
 
 
 func lostNumbers(from list: [Int]) throws -> [Int] {
     let list = list.sorted(by: <)
 
-    if list.count <= 2 {
-        throw NumberError.notEnoughElements
+    // Checking errors
+    if list.count <= 1 {
+        throw ListError.notEnoughItems
     }
 
     var lastNum = Int()
 
     for num in list {
-        if num == lastNum { throw NumberError.duplicate }
+        if num == lastNum { throw ListError.duplicateItems }
 
         lastNum = num
     }
 
-    var missing = [Int]()
-    
-    if let first = list.first, let last = list.last {
-        for num in first...last {
-            if list.contains(num) { continue }
+    // Finding lost numbers
+    let first = list.first!
+    let last = list.last!
+    var lost = [Int]()
 
-            missing.append(num)
-        }
+    for num in first...last {
+        if list.contains(num) { continue }
+
+        lost.append(num)
     }
 
-    return missing
+    return lost
 }
 
+
 do {
-    try print(lostNumbers(from: [10, 18, 14]))
-} catch NumberError.duplicate {
+    try print(lostNumbers(from: [3, 5, 7, 10, 12, 14]))
+    try print(lostNumbers(from: [16, 18, 20, 22, 23, 25]))
+    try print(lostNumbers(from: [51, 54, 57, 60, 62, 63]))
+    // try print(lostNumbers(from: [7]))
+    // try print(lostNumbers(from: [20, 20]))
+}
+catch ListError.duplicateItems {
     print("The list contains duplicate numbers.")
-} catch NumberError.notEnoughElements {
-    print("There are not enough elements.")
+}
+catch ListError.notEnoughItems {
+    print("There are not enough numbers.")
 }
 
