911374b9b37f79b535408eb923dd5d1413f23fc8
Eduardo Hernand <byhernand@hotmail.com>
2023-06-12 22:38:56 -0600

    Updated filter out strings

--- filter-out-strings.playground/Contents.swift
+++ filter-out-strings.playground/Contents.swift
@@ -10,16 +10,27 @@ import Foundation
 
 
 func filterArray(_ list: [Any]) {
-    let filteredList = list.filter {
+    let integersList = list.filter {
         let item = type(of: $0)
         let isInt = type(of: item) == Int.Type.self
         return isInt
     }
-    
+
+    var filteredList = [Int]()
+
+    integersList.forEach {
+        if let integer = Int("\($0)") {
+            if !filteredList.contains(integer) {
+                filteredList.insert(integer, at: filteredList.endIndex)
+            }
+        }
+    }
+
     print(filteredList)
 }
 
 
 filterArray([1, 2, "a", "b"])
+filterArray([1, 2, 2, 3, "a", "b"])
 filterArray([1, "a", "b", 0, 15])
 filterArray([1, 2, "aasf", "1", "123", 123])
